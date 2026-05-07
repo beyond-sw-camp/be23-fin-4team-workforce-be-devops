5f41cbc411aa00c31a4d72082a7f91d5914c8fdb
semin <tpalsdl87@naver.com>
2026-05-08 02:15:04 +0900

    14트

--- search-service/src/main/java/com/_team/_team/SearchApplication.java
+++ search-service/src/main/java/com/_team/_team/SearchApplication.java
@@ -2,10 +2,10 @@ package com._team._team;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com._team._team")
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class SearchApplication {
 
 	public static void main(String[] args) {
